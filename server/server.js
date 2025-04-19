d90d671b73246d616ea966d822d598a897e4e35e
Tusar Manna <tusarmanna2003@gmail.com>
2025-04-19 13:01:13 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ const corsOptions = {
   },
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
-  credentials: true,
+  credentials: false,
   maxAge: 86400
 };
 
